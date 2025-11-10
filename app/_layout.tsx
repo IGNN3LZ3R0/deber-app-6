de2bef728c86d54a34d1b01af06e1937a63506a9
Juan Carlos <juan.gonzalez02@epn.edu.ec>
2025-11-10 16:40:44 -0500

    Primer Commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+import { useAuth } from "../src/presentation/hooks/useAuth";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const { usuario, cargando } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (cargando) return;
+
+    const enAuth = segments[0] === "auth";
+
+    if (!usuario && !enAuth) {
+      router.replace("/auth/login");
+    } else if (usuario && enAuth) {
+      router.replace("/(tabs)");
+    }
+  }, [usuario, segments, cargando]);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="auth" options={{ headerShown: false }} />
+    </Stack>
   );
 }
